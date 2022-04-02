eb15e5d8535d7cfea8a76d034e4aaeccb1ca8977
Kauã <kau.lira@yahoo.com>
2022-04-01 22:17:49 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿double numerador, denominador, quociente; 
+
+Console.Write("Digite seu numerador: "); 
+numerador = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite seu dominador: ");
+denominador = Convert.ToDouble(Console.ReadLine());
+
+if(denominador ==0)
+{   
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("Não é possivel dividir por zero");    
+}
+
+else 
+{    
+   quociente = numerador/denominador;
+
+   Console.WriteLine($"{numerador:n1} dividido por {denominador:n1} seria {quociente}");
+   Console.Beep(); 
+}
+
+Console.ResetColor();
